8a3d1b864162341638f2d8594b80d8ee3a9419fd
Alex Cyrillo <alex.csborges@hotmail.com>
2023-10-18 20:27:03 -0300

    update: dependencias

--- 1.aluraflix/server.js
+++ 1.aluraflix/server.js
@@ -2,5 +2,5 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-	console.log(`Servidor rodando na porta  ${port}`);
+	console.log(`Servidor rodando na porta :${port}`);
 });
